07f2e335aa8c4abc1188b9f93eeb3099a2657e78
Tasneem Wadee <tasneemw026@gmail.com>
2023-02-10 07:25:49 +0000

    Update 5 files

---
+++ src/components/FunctionClick.js
@@ -0,0 +1,27 @@
+import React from "react";
+import MyForm from './MyForm'
+
+function FunctionClick(){
+
+
+ function warning(){
+     console.log('button was clicked')
+    alert("THE ALIENTS ARE COMINGGGGG") ;
+   <MyForm/>
+    }
+
+ 
+  return (
+   
+    <div>
+      <h2> Hello Earthlings </h2>
+   
+    
+       <button  onClick={warning}> Aliens!! </button>
+    
+    </div>
+  );
+
+}
+
+export default FunctionClick
